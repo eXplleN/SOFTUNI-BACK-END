6fdc2f44171c89e9da89f4a6b813e3f6594a5f61
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-01-28 19:40:32 +0200

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import homeController from './controllers/homeController.js';
+
+const routes = Router();
+
+routes.use(homeController);
+
+routes.get('*', (req, res) => {
+    res.render('404');
+});
+
+
+export default routes;
