0d36a30c7ebee12567e0b1d3ce3b29342d54c5c8
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-01-28 20:35:18 +0200

    Add movie service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 const movieContorller = Router();
 
@@ -7,6 +8,9 @@ movieContorller.get('/create', (req, res) => {
 });
 
 movieContorller.get('/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+
+    const movie = movieService.findMovie(movieId);
     res.render('details');
 });
 
---
+++ src/services/movieService.js
@@ -0,0 +1,10 @@
+import movies from "../movies.js";
+
+export default {
+    findMovie(movieId) {
+         const result = movies.find(movie => movie.id === movieId);
+    
+         return result;
+    }
+    
+}
