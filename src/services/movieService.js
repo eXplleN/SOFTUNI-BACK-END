04bed2bb5971ab07af498cf43d92c215eb36827f
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-02 13:50:58 +0200

    Add search by genre and year

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,6 +8,14 @@ export default {
         if (filter.search) {
            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
         }
+
+        if (filter.genre) {
+            result = result.filter( movie => movie.genre.toLowerCase() === filter.genre);
+        }
+
+        if (filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
         return movies;
     },
     findMovie(movieId) {
