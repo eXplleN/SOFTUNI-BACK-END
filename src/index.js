277ba8664d39959775d2b3680edb715f2f1ff437
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-05 18:17:13 +0200

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import routes from './routes.js';
 import showRatingHelper from './helpers/ratingHelper.js';
+import mongoose from 'mongoose';
 
 const app = express();
+
+try {
+    const uri = 'mongodb://localhost:27017/magic-movies-jan2025';
+    await mongoose.connect(uri);
+    
+    console.log('DB connected successfully!');
+    
+}catch (err) {
+    console.log('Cannot connect to DB!');
+    console.error(err.message);
+}
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
