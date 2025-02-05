8207b97ffa1a7b63150150bed0e7fc27d3c51bb3
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-05 20:03:27 +0200

    Add create cast page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import movieContorller from './controllers/movieController.js';
+import castController from './controllers/castController.js';
 
 const routes = Router();
 
 routes.use(homeController);
 routes.use('/movies', movieContorller);
+routes.use('/casts', castController);
 
 routes.get('*', (req, res) => {
     res.render('404');
