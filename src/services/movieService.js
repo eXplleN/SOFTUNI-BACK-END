e3edae80338aa0f4d16b6f36ff1b4cf1d2a52c74
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-07 16:50:03 +0200

    Add movie cast relationship

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -30,5 +30,12 @@ export default {
         });
 
         return result;
+    },
+     async attachCast(movieId, castId){
+        const movie =  await Movie.findById(movieId);
+        movie.casts.push(castId)
+        await movie.save();
+
+        return movie;
     }
 }
