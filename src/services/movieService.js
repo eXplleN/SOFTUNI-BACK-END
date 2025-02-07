587332a10d543dcb9fa110332df942e1f22aff10
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-07 17:54:43 +0200

    Modified update casts query

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -35,10 +35,12 @@ export default {
         return result;
     },
      async attachCast(movieId, castId){
-        const movie =  await Movie.findById(movieId);
-        movie.casts.push(castId)
-        await movie.save();
+        // const movie =  await Movie.findById(movieId);
+        // movie.casts.push(castId)
+        // await movie.save();
 
-        return movie;
+        // return movie;
+
+        return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
     }
 }
