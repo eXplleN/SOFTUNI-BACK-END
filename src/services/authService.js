e81b8f9caff7c03676b7ff7a9e1b5461c52a8334
Georgi Georgiev <georgi.georgiev.1991@abv.bg>
2025-02-11 18:59:30 +0200

    Add login functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,32 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'a2bb10tOOeTb5OetVVqdmQvLCOLnUUnI43MCyZLtpGlGefzqcIo0Zy';
 
 export default {
-    register(userData){
+    register(userData) {
         
         return User.create(userData);
+    },
+    async login(email, password) {
+       
+        const user = await User.findOne({email});
+        if(!user) {
+            throw new Error('Invalid email or password');
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if(!isValid) {
+            throw new Error('Invalid email or password');
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+        }
+        const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+        return token
     }
 }
